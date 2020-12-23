b92ad1ad38becbf6f623d733a618fcf6d1ba47df
Simon Panek <paneks19@gmail.com>
2020-12-22 18:10:00 -0800

    all parts functioning, but duplicate messages

--- caps/caps.js
+++ caps/caps.js
@@ -15,29 +15,32 @@ const caps = io.of('/caps');
 console.log('CAPS, reporting for duty!');
 
 io.on('connection', (socket) => {
-  console.log('Connected to CAPS with socket ID: ', socket.id);
+// console.log('Connected to CAPS with socket ID: ', socket.id);
 
   caps.on('connection', (socket2) => {
-    console.log('Connected to CAPSNameSpace with socket ID: ', socket2.id);
+ // console.log('Connected to CAPSNameSpace with socket ID: ', socket2.id);
 
-    socket2.on('pickup', (payload) => console.log('pickup @ CAPS', payload)); //eventLogger('pickup', payload)); //listener hears pickup calls eventLogger
-    caps.on('in-transit', (payload) => eventLogger('in-transit', payload));
-    caps.on('delivered', (payload) => eventLogger('delivered', payload));
+    socket2.on('pickup', (payload) => eventLogger('pickup', payload)); //listener hears pickup calls eventLogger
+    socket2.on('in-transit', (payload) => eventLogger('in-transit', payload));
+    socket2.on('delivered', (payload) => eventLogger('delivered', payload));
 
     function eventLogger (event, payload) {
-
+// console.log('inside eventLogger. event: ', event, 'payload ', payload);
       if(event === 'delivered') { //if incoming is delivered send out delivered-ready
         caps.emit('delivered-ready', payload);
       }
 
       const time = Date(); //time stamp
-      console.log(`Event , { event: ${event}, time: ${time}, payload: ${util.inspect(payload, { showHidden: false, depth: null})}`); //log the event
+      console.log(`Event { 
+        event: ${event}, 
+        time: ${time}, 
+        payload: ${util.inspect(payload, { showHidden: false, depth: null})}`); //log the event
 
       if(event === 'delivered-ready'){//if incoming is delivered append break for ease of review
         console.log('------------------Transaction Break------------------');
       }
 
-      if(event === 'pickup-ready'){//if incoming is pickup send out pickup-ready
+      if(event === 'pickup'){//if incoming is pickup send out pickup-ready
         caps.emit('pickup-ready', payload);
       }
     }
