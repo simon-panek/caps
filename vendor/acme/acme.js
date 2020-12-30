b2da920befd8c218ac98de8c59d34aa4473a6677
Simon Panek <paneks19@gmail.com>
2020-12-30 00:05:35 -0800

    all client message queues working

--- vendor/acme/acme.js
+++ vendor/acme/acme.js
@@ -8,7 +8,7 @@ const socket = io.connect(`${host}/caps`); //connecting to the caps name space
 
 const storeName = 'acme-widgets'; //store name
 
-let messageChecker; //declare a switch variable for controlling the confirmation message
+//let messageChecker; //declare a switch variable for controlling the confirmation message
 
 console.log('Widget vendor, reporting for duty!');
 
@@ -22,9 +22,11 @@ socket.emit('get-all', vendorPayload); //retrieve all messages in queue
 socket.on('messageQ', message => { //receiving queued messages from Q
   // console.log('A-CSL#1 in queued messages heard - MESSAGEQ: ', message);
   if(message.payload.event === 'delivered') { //if the event in the queued message is 'delivered' call the thankyou function with the payload
-    thankYou(message.payload.payload, 0);
+    thankYou(message.payload);
   }
-  socket.emit('received', message.id); //respond to Q that message was received and delete
+  if(message.payload.event === 'in-transit') { //if the event in the queued message is 'delivered' call the thankyou function with the payload
+    socket.emit('received', message.id); //respond to Q that message was received and delete
+  }
 });
 
 socket.on('in-transit', message => { //listen for in-transit
@@ -46,13 +48,11 @@ setInterval(() => { //create new payload every 5 seconds
 
 socket.on('delivered', thankYou); //listen for delivered and fire thankYou
 
-function thankYou(message, messageChecker=1){
-  // console.log('A-CSL#3 ThankYou ', message);
-  console.log(`VENDOR: Thank you for delivering ${message.payload.payload.orderID}`); //log thankyou & order ID
+function thankYou(message){
+console.log('A-CSL#3 ThankYou ', message);
+  console.log(`VENDOR: Thank you for delivering ${message.payload.orderID}`); //log thankyou & order ID
 
-  if(messageChecker === 1) { //check if function was called from live listener or from queued message
-    socket.emit('received', message.id); //respond with confirmation message
-  }
+  socket.emit('received', message.id); //respond with confirmation message
 }
 
 module.exports = thankYou;
--- vendor/flowers/flowers.js
+++ vendor/flowers/flowers.js
@@ -8,7 +8,7 @@ const socket = io.connect(`${host}/caps`); //connecting to the caps name space
 
 const storeName = '1-206-Flowers'; //store name
 
-let messageChecker; //declare a switch variable for controlling the confirmation message
+//let messageChecker; //declare a switch variable for controlling the confirmation message
 
 console.log('Flower vendor, reporting for duty!');
 
@@ -22,7 +22,10 @@ socket.emit('get-all', vendorPayload); //retrieve all messages in queue
 socket.on('messageQ', message => { //receiving queued messages from Q
   // console.log('F-CSL#1 in queued messages heard - MESSAGEQ: ', message);
   if(message.payload.event === 'delivered') { //if the event in the queued message is 'delivered' call the thankyou function with the payload
-    thankYou(message.payload.payload);
+    thankYou(message.payload);
+  }
+  if(message.payload.event === 'in-transit') { //if the event in the queued message is 'delivered' call the thankyou function with the payload
+    socket.emit('received', message.id); //respond with confirmation message
   }
   //socket.emit('received', message.id); //respond to Q that message was received and delete
 });
@@ -47,8 +50,8 @@ setInterval(() => { //create new payload every 5 seconds
 socket.on('delivered', thankYou); //listen for delivered and fire thankYou
 
 function thankYou(message){
-  // console.log('F-CSL#3 ThankYou ', message);
-  console.log(`VENDOR: Thank you for delivering ${message.payload.payload.orderID}`); //log thankyou & order ID
+console.log('F-CSL#3 ThankYou ', message);
+  console.log(`VENDOR: Thank you for delivering ${message.payload.orderID}`); //log thankyou & order ID
 
   socket.emit('received', message.id); //respond with confirmation message
 
